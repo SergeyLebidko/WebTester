e480001db270877fa82fd36e6638655063619e60
SergeyLebidko <it.sergeyler@mail.ru>
2019-10-30 20:05:38 +0300

    Создан код отображения списка тем и списка тестов

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, test_list
 
 app_name = 'main'
 
 urlpatterns = [
+    path('<int:test_group_id>/', test_list, name='test_list'),
     path('', index, name='index')
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+from .models import TestGroup, Test
 
 
+# Контроллер главной страницы
 def index(request):
-    return render(request, 'main/index.html', {})
+    test_groups = TestGroup.objects.all()
+    context = {'test_groups': test_groups}
+    return render(request, 'main/index.html', context)
+
+
+# Контроллер страницы со списком тестов
+def test_list(request, test_group_id):
+    try:
+        selected_group = TestGroup.objects.get(pk=test_group_id)
+    except (TestGroup.DoesNotExist, TestGroup.MultipleObjectsReturned):
+        return HttpResponseBadRequest()
+    tests = Test.objects.filter(test_group=selected_group)
+    context = {'tests': tests, 'selected_group': selected_group}
+    return render(request, 'main/test_list.html', context)
